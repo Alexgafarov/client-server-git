a116508de35a9d4ca77f2a9aba2e89399d5fc882
Alex <algafarov-1@yandex.ru>
2021-06-06 13:51:33 +0700

    init project

---
+++ client-server/Lesson 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-server
+//
+//  Created by Мария Гафарова on 06.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
